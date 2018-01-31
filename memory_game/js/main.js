9c428b9c51ad6ee333dbd686591a9d50005c13ee
Allyson Finck <allysonfinck@Allysons-iMac.local>
2018-01-31 14:31:41 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,30 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+};
 
-cardsInPlay.push('cardOne');
-cardsInPlay.push('cardTwo');
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
 
-console.log("User flipped queen");
-console.log("User flipped king");
+	cardsInPlay.push(cards[cardId]);
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match! ");
-} else {
-	alert("Sorry, try again. ");
-}
+	checkForMatch();
+
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match! ");
+	} else {
+		alert("Sorry, try again. ");
+	}
+};
+
+flipCard(0);
+flipCard(2);
+
+
+
